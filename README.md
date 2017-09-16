50b556964e758424566971fb53f0c7ec5d53a53b
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-09-16 16:45:14 -0700

    Add crashlytics

---
+++ README.md
@@ -0,0 +1,9 @@
+## Setup
+
+Create a `/app/fabric.properties` file and set the following
+variables using your Fabric keys.
+
+```
+apiSecret=
+apiKey=
+```
--- app/src/main/java/la/melvin/mobile/MainActivity.kt
+++ app/src/main/java/la/melvin/mobile/MainActivity.kt
@@ -2,11 +2,16 @@ package la.melvin.mobile
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
